c6fe8a7d1cec660d266e4c2bb02b6f14c9f67cdb
indra-uolles <smirnovanatalia2008@gmail.com>
2015-08-18 10:30:08 +0300

    finished with widget data settings

--- my_gallery.js
+++ my_gallery.js
@@ -1,60 +1,23 @@
-//$($('.photo-gallery')[0]).find("[data-initial-slide=true]")
-// (function($) {
-//   $.fn.myGallery.defaults = {
-//     foreground: "red",
-//     background: "yellow"
-//   };
-//   var methods = {
-//     init: function(options) { 
-//       return this.each(function(options){
-         
-//         var $this = $(this),
-//           data = $this.data('gallery');
-         
-//         // Если плагин ещё не проинициализирован
-//         if (!data) {
-//           /*
-//           * Тут выполняем инициализацию
-//           */
-
-//           $(this).data('gallery', {
-//             target: $this
-//           });
-//           $(this).data('options', {
-//             options: options;
-//           });
-
-//         }
-//       });
-//     },
-//     showNext: function() {
-//       //alert $(this).data;
-//       console.log('ha');
-//     },
-//     showPrev: function( ) {
-//     }
-//   };
-//   $.fn.myGallery = function(method, options) {
-//     if (typeof(options) == 'undefined') {
-//       options = {};
-//     }
-//     if (methods[method]) {
-//       return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-//     } else if (typeof method === 'object' || !method) {
-//       return methods.init.apply(this, arguments);
-//     } else {
-//       $.error('Метод с именем ' +  method + ' не существует для jQuery.myGallery');
-//     }    
-//   };
-// })(jQuery);
-
 $.widget("custom.mygallery", {
  
   options: {
     count: 1
   },
 
+  _create: function() {
+    this.options = $.extend( {
+      nextIndex: 0,
+      prevIndex: this.options.count - 1
+    }, this.options);
+  },
+
   showNext: function() {
     console.log(this.options.count);
+    console.log(this.options.prevIndex);
+    //var initialSlides = this.element.find("[data-initial-slide=true]");
+  },
+
+  showPrev: function() {
+
   }
 });
