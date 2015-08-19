638a1ff688bd5450ce0cb250264dc0525aa7f646
indra-uolles <smirnovanatalia2008@gmail.com>
2015-08-19 08:02:04 +0300

    calculate need to disable prev and next

--- my_gallery.js
+++ my_gallery.js
@@ -8,8 +8,7 @@ $.widget("custom.mygallery", {
     var slides = this.element.find('.photo-gallery__slides');
 
     this.options = $.extend({
-      nextIndex: 0,
-      prevIndex: slides.length - 1,
+      currIndex: 0,
       slides: slides
     }, this.options);
 
@@ -21,6 +20,33 @@ $.widget("custom.mygallery", {
         this._slidePrev();
       },
     });
+
+    this._togglePrevState();
+    this._toggleNextState();
+  },
+
+  _toggleNextState: function() {
+    var index = this.options.currIndex + this.options.count,
+        left  = this._ifSlidesLeft(index),
+        element = this.element.find('.photo-gallery__next');
+
+    if (left == false) {
+      element.addClass('is-disabled');
+    } else {
+      element.removeClass('is-disabled');
+    }
+  },
+
+  _togglePrevState: function() {
+    var index = this.options.currIndex - this.options.count,
+        left  = this._ifSlidesLeft(index, false),
+        element = this.element.find('.photo-gallery__prev');
+
+    if (left == false) {
+      element.addClass('is-disabled');
+    } else {
+      element.removeClass('is-disabled');
+    }
   },
 
   _slideNext: function() {
@@ -28,12 +54,39 @@ $.widget("custom.mygallery", {
 
     var value = this._slideValue($slides, true);
     $slides.transition({ x: value }, 1000);
+
+    this._increaseCurrIndex();
+    this._togglePrevState();
+    this._toggleNextState();
   },
 
   _slidePrev: function() {
     var $slides = this.options.slides;
     var value = this._slideValue($slides, false);
     $slides.transition({ x: value }, 1000);
+
+    this._decreaseCurrIndex();
+    this._togglePrevState();
+    this._toggleNextState();
+  },
+
+  _ifSlidesLeft: function(index, forward) {
+    if (typeof(forward) == 'undefined') {
+      forward = true;
+    }
+    var result = false;
+
+    if (forward == true) {
+      left = this.options.slides.find('.photo-gallery__slide').length - index;
+    } else {
+      left = index;
+    }
+
+    if ((forward == true && left > 0) || (forward == false && left >= 0)) {
+      result = true;
+    }
+
+    return result;
   },
 
   _slideValue: function(element, next) {
@@ -44,5 +97,13 @@ $.widget("custom.mygallery", {
     } else {
       return "+=" + slideWidth;
     }
+  },
+
+  _increaseCurrIndex: function() {
+    this.options.currIndex += this.options.count;
+   },
+
+  _decreaseCurrIndex: function() {
+    this.options.currIndex -= this.options.count;
   }
 });
